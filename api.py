7e9a5ee02d6508d3299a40d4966d297a0b451e78
Robert Putt <Robert.Putt@C02FV3BTMD6R.local>
2023-07-04 21:07:57 +0100

    Add facilities list

---
+++ api.py
@@ -0,0 +1,27 @@
+import boto3
+import json
+
+from flask import Flask
+from flask import jsonify
+
+
+app = Flask(__name__)
+
+
+BUCKET = "aggregator-nhs-ed-wait-times-data"
+
+
+@app.route("/")
+def get_app_version():
+    app_data = {
+        "service": "NHS Emergency Department Wait Times Aggregator API",
+        "version": 1.0
+    }
+    return jsonify(app_data)
+
+
+@app.route('/facilities')
+def get_facilities():
+    s3 = boto3.resource('s3')
+    obj = s3.Object(BUCKET, 'facilities.json')
+    return jsonify(json.loads(obj.get()['Body'].read().decode('utf-8')))
