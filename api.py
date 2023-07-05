d10179a29480fe1d43a9d160a453e5a30b7d48ee
Robert Putt <Robert.Putt@C02FV3BTMD6R.local>
2023-07-05 11:16:09 +0100

    Get last 24 hours of data by default

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 import boto3
 import json
+import datetime
 
 from flask import Flask
 from flask import jsonify
@@ -7,6 +8,41 @@ from flask import jsonify
 
 app = Flask(__name__)
 BUCKET = "aggregator-nhs-ed-wait-times-data"
+
+
+def get_facility_by_id(facility_id):
+    s3 = boto3.resource('s3')
+    obj = s3.Object(BUCKET, 'facilities.json')
+    data = json.loads(obj.get()['Body'].read().decode('utf-8'))
+    selected_facility = None
+
+    for facility in data.get('facilities'):
+        if facility.get('id') == facility_id:
+            selected_facility = facility
+
+    return selected_facility
+
+
+def get_data_by_range(facility_id, start_date, end_date):
+    date_diff = end_date - start_date
+    days_diff = date_diff.days
+    data = []
+    for x in range(0, days_diff + 1):
+        current_date = start_date + datetime.timedelta(days=x)
+        data.extend(get_data_by_date(facility_id, current_date))
+    return data
+
+
+def get_data_by_date(facility_id, date):
+    s3 = boto3.resource('s3')
+    obj = s3.Object(BUCKET, '%s/%s/%s/%s.json' % (
+        facility_id,
+        date.year,
+        date.month,
+        date.day
+    ))
+    data = json.loads(obj.get()['Body'].read().decode('utf-8'))
+    return data
 
 
 @app.route("/")
@@ -23,3 +59,22 @@ def get_facilities():
     s3 = boto3.resource('s3')
     obj = s3.Object(BUCKET, 'facilities.json')
     return jsonify(json.loads(obj.get()['Body'].read().decode('utf-8')))
+
+
+@app.route('/facilities/<string:facility_id>')
+def get_facility(facility_id):
+    facility = get_facility_by_id(facility_id)
+
+    if not facility:
+        return 'Not Found', 404
+
+    facility['data'] = []
+
+    # By default get last 24 hours of readings...
+    now = datetime.datetime.utcnow()
+    start_date = now - datetime.timedelta(hours=24)
+    end_date = now
+    data = get_data_by_range(facility_id, start_date, end_date)
+    facility['data'].append(data)
+
+    return jsonify(facility)
